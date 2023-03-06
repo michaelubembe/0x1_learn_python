2337210541a0e9661e2081379ae2ce4eb08dd964
Lubembe Michael <lubemb@gmail.com>
2023-03-06 11:18:18 +0300

    more print string manipulation

--- 0x00_manipulating_strings.py
+++ 0x00_manipulating_strings.py
@@ -1,6 +1,6 @@
 a = 'Python is cool'
 print(a[7:-5]) #prints is
-print(a[7:]) #prints is cool
+print(a[7:]) #last 7 is cool
 print('Normal string') #prints Normal string
 print(f'{98} Battery street') #prints 98 Battery street
 print(a[0:6]) #prints Python
@@ -8,3 +8,4 @@ print(a[1:3]) #prints yt
 print(a[4:3]) #prints nothing
 print(a[4:5]) #prints o
 print(a[:-1]) #prints Python is coo
+print(a[:7]) #prints first 7
